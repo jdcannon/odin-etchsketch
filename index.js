90f61dc49da48f2c77ba23fa73f845d5a1f27e87
James Cannon <promethorn@outlook.com>
2025-02-06 15:15:44 -0500

    Added persistent coloring

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < 256; i++){
   const div = document.createElement("div");
+  div.addEventListener("mouseenter", (e) => div.classList.add("drawn"));
   container.appendChild(div);
 }
