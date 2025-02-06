6d203adaa10697422833876133b643149ed12f09
James Cannon <promethorn@outlook.com>
2025-02-06 16:08:48 -0500

    Finished main project

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 256; i++){
-  const div = document.createElement("div");
-  div.addEventListener("mouseenter", (e) => div.classList.add("drawn"));
-  container.appendChild(div);
+function generateGrid(sideLength) {
+  container.replaceChildren();
+  let squares = sideLength*sideLength;
+  let squareSize = 100 / sideLength;
+
+  if (sideLength > 100 ) {
+    squares = 10000;
+    squareSize = 1;
+  }
+  for (let i = 0; i < squares; i++){
+    const div = document.createElement("div");
+    div.style.height = squareSize.toString() + "%";
+    div.style.width = squareSize.toString() + "%";
+    div.addEventListener("mouseenter", (e) => div.classList.add("drawn"));
+    container.appendChild(div);
+  }
 }
+function newGrid() {
+  const squareEntry = document.querySelector("#squares");
+  const value = parseInt(squareEntry.value);
+  generateGrid(value);
+}
+document.querySelector("#new-canvas").addEventListener("click", newGrid);
+
+generateGrid(24);
